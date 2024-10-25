b7ae9e0d04cac0296d9342ba005d64702049490b
cejasmiguel16 <42989205+cejasmiguel16@users.noreply.github.com>
2024-10-25 14:49:13 -0300

    Practico 4 - completado

---
+++ practico-4/views/responseView.mjs
@@ -0,0 +1,8 @@
+export function renderizarSuperheroe(superheroe) {
+    return JSON.stringify(superheroe, null, 2)
+}
+
+export function renderizarListaSuperheroes(superheroes) {
+    return JSON.stringify(superheroes, null, 2)
+}
+
